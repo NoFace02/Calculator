d579358097f2c3d2d81ad09d281ab41e9e1d7f44
NoFace02 <mariandemetter2@gmail.com>
2024-12-07 21:08:26 +0200

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -10,7 +10,20 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            asd
+            //First program how to do variables
+                string characterName = "";
+                int characterDicks;
+                characterDicks = 35;
+
+                characterName = "John";
+                Console.WriteLine("There was a dick named " + characterName);
+                Console.WriteLine("He had " + characterDicks + " dicks in his mounth");
+                characterName = "Nick";
+                Console.WriteLine("He really Liked the name " + characterName);
+                Console.WriteLine("But he didnt like having " + characterDicks + " dicks in his mounth");
+
+                Console.ReadLine();
+
         }
     }
 }
